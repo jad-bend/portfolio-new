10bd4cbb23fd0d6b9a261f7e0d5e7f55c57d2dd1
Jad Bendarkawi <jadb@princeton.edu>
2024-07-11 21:15:41 -0700

    maintenance

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,6 @@ import RightSidebar from "./RightSidebar";
 import { Fragment, ReactNode } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { Breadcrumbs } from "./page";
 
 const inter = Inter({ subsets: ["latin"] });
 
